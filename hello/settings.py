a854ba85bdaa21d5ca5b50f7d0683f6490daf26e
adi7722 <adnaan7722@hotmail.com>
2024-10-27 04:38:25 +0500

    POSTGRESS DATABASE ADDED

--- hello/settings.py
+++ hello/settings.py
@@ -91,8 +91,12 @@ WSGI_APPLICATION = 'hello.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'PearlFoods',
+        'USER': 'admin_pearl_foods',
+        'PASSWORD': 'MOH9e]52>0@h4l%',
+        'HOST': 'localhost',  # or your database host
+        'PORT': '5432',       # default PostgreSQL port
     }
 }
 
