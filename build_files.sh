763e3561a237af870a2b59464273406ce558fa96
adi7722 <adnaan7722@hotmail.com>
2024-10-27 04:05:01 +0500

    vercel

--- build_files.sh
+++ build_files.sh
@@ -3,10 +3,10 @@
 echo "BUILD START"
 
 # Upgrade pip and install dependencies
-python -m pip install --upgrade pip
-python -m pip install -r requirements.txt
+python3 -m pip install --upgrade pip
+python3 -m pip install -r requirements.txt
 
 # Collect static files
-python manage.py collectstatic --noinput --clear
+python3 manage.py collectstatic --noinput --clear
 
 echo "BUILD END"
