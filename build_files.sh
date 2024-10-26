198e64edd1d347a8c6433f83d650c134c12fde58
adi7722 <adnaan7722@hotmail.com>
2024-10-27 04:53:08 +0500

    POSTGRESS DATABASE ADDED

--- build_files.sh
+++ build_files.sh
@@ -2,14 +2,23 @@
 
 echo "BUILD START"
 
-# Export database environment variable for Django to use PostgreSQL
+# Set environment variables for PostgreSQL database
 export DATABASE_URL="postgres://admin_pearl_foods:MOH9e]52>0@h4l%@localhost:5432/PearlFoods"
+export DATABASE_USER="admin_pearl_foods"
+export DATABASE_PASSWORD="MOH9e]52>0@h4l%"
+export DATABASE_NAME="PearlFoods"
+export DATABASE_HOST="localhost"
+export DATABASE_PORT="5432"
+
+# Add the Python script directory to PATH for any necessary executables
+export PATH=$PATH:/python312/bin
 
 # Upgrade pip and install dependencies
 python3 -m pip install --upgrade pip
-python3 -m pip install -r requirements.txt
+python3 -m pip install -r requirements.txt --no-warn-script-location
 
-# Collect static files
+# Run database migrations and collect static files
+python3 manage.py migrate
 python3 manage.py collectstatic --noinput --clear
 
 echo "BUILD END"
